71b81581d0dffacfdc4a8dd991ddc237eee33ab6
Ayush <sahuayush359@gmail.com>
2023-07-05 19:07:19 +0530

    first commit

---
+++ src/main/java/com/in/blog/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.in.blog.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint{
+
+	
+	//Commence method will execute when unauthorized user is used to access the authorized api
+	@Override
+	public void commence(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException authException) throws IOException, ServletException {
+		
+		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "ACCESS DENIED!");
+		
+	}
+	
+
+}
